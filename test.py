b4ac621025e249b6bc2595dbd86577961b60e6ca
keisuke-ono <kekechi730@gmail.com>
2024-02-12 12:17:11 +0900

    機能追加

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ def reverse_totuple(ln):
     ln.reverse()
     return tuple(ln)
 
+def add():
+    prtin("add")
+
 print(change_domain('spam@utokyo-ipp.org', 'ipp.u-tokyo.ac.jp') == 'spam@ipp.u-tokyo.ac.jp')
 print(change_domain('spam@utokyo-ipp.org', 'ipp.u-tokyo.ac.jp'))
 print(reverse_totuple([1, 2, 3, 4, 5]) == (5, 4, 3, 2, 1))
